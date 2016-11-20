a330620adf1b945387f513747075ca43a6a6c265
Qi Wang <jamie@guest-wireless-upc-1603-10-120-060-064.usc.edu>
2016-11-19 21:15:08 -0800

    add more to detector

--- Detector.cpp
+++ Detector.cpp
@@ -2,10 +2,17 @@
 
 
 bool similar(char* row1, int pos1, char* row2, int pos2) {
-  if( abs(row1[pos1]-row2[pos2]) +  abs(row1[pos1 + 1]-row2[pos2 + 1]) + abs(row1[pos1 + 2]-row2[pos2 + 2]) < 12 ) {
+  if( abs(row1[pos1]-row2[pos2]) +
+      abs(row1[pos1 + 1]-row2[pos2 + 1]) +
+      abs(row1[pos1 + 2]-row2[pos2 + 2]) < 12 ) {
     return true;
   }
   else return false;
+}
+
+float GetFloatPrecision(float value, float precision)
+{
+    return (floor((value * pow(10, precision) + 0.5)) / pow(10, precision));
 }
 
 bool ShotBoundaryDetector::StartDetection() {
@@ -23,42 +30,49 @@ bool ShotBoundaryDetector::StartDetection() {
     return false;
   }
 
-  int num_frame = 450;
-
-  MyImage tomograph;
-  tomograph.setWidth(Width);
-  tomograph.setHeight(num_frame);
-  tomograph.setImagePath("tomograph.rgb");
-  char* tomoData = new char[tomograph.getWidth()*tomograph.getHeight()*3];
-
-  long frame_id = 1;
-  // while(!feof(IN_FILE))
+  vector<vector<char> > tomoData_r;
+  vector<vector<char> > tomoData_g;
+  vector<vector<char> > tomoData_b;
 
   char* avg_row_prev = new char[Width*3];
   char* avg_row_curr = new char[Width*3];
 
-  vector<int> counts(num_frame, 0);
+  vector<int> counts;
+  vector<float> dc;
+  vector<float> ddc;
+  float fade_begin_dc = -1;
 
-  for(int i = 0; i < num_frame; i++)
+  ofstream myfile1;
+  ofstream myfile2;
+  myfile1.open ("ddc-y.txt");
+  myfile2.open ("ddc-x.txt");
+
+  int i = 0;
+  while ( !feof(IN_FILE) )
   {
+    counts.push_back(1);
+
     MyImage frame;
     frame.setWidth(Width);
     frame.setHeight(Height);
+
     stringstream ss;
-    ss << "frame_" << frame_id<<".rgb";
+    ss << "frame_" << i<<".rgb";
     frame.setImagePath(ss.str().c_str());
+
     if(!ReadNextFrame(frame, IN_FILE)) return false;
-    //if(frame_id % 90 == 0) frame.WriteImage();
-    frame_id ++;
 
     if(!GetAvgRow(frame,avg_row_curr)) return false;
 
+    tomoData_r.push_back(vector<char>(0));
+    tomoData_g.push_back(vector<char>(0));
+    tomoData_b.push_back(vector<char>(0));
+
     for(int col = 0; col < Width; col++) {
-      int pic_position = i * Width * 3 + col * 3;
       int row_position = col * 3;
-      tomoData[pic_position] = avg_row_curr[row_position];
-      tomoData[pic_position + 1] = avg_row_curr[row_position + 1];
-      tomoData[pic_position + 2] = avg_row_curr[row_position + 2];
+      tomoData_r[i].push_back( avg_row_curr[row_position] );
+      tomoData_g[i].push_back( avg_row_curr[row_position + 1] );
+      tomoData_b[i].push_back( avg_row_curr[row_position + 2] );
 		}
 
     char* t = avg_row_prev;
@@ -78,26 +92,50 @@ bool ShotBoundaryDetector::StartDetection() {
       }
     }
 
+    if(i >= 1) dc.push_back(1.0 * counts[i] / counts[i-1]);
+    if(i >= 2) {
+      ddc.push_back(dc.back() / dc[dc.size()-2]);
+      myfile1<< GetFloatPrecision(ddc.back(),3) <<endl; // ddc val
+      myfile2<<( (i%3==0)? 1.0*i/30 : i * 0.0333)<<endl; // time
+
+      if(fade_begin_dc < 0) {
+        if(ddc.back() > 20) {
+            //find a hard cut
+            frame.WriteImage();
+        }
+        else if(ddc.back() > 5) {
+          fade_begin_dc = dc[dc.size()-2];
+        }
+      }
+      else {
+        if( dc.back() / fade_begin_dc > 20) {
+          fade_begin_dc = -1;
+          //find a fade cut
+          frame.WriteImage();
+        }
+      }
+
+    }
+    i++;
   }
 
-  vector<float> dc(num_frame, 0);
-  for(int i = 1; i < num_frame; i++) {
-    dc[i] = counts[i-1] !=0 ? 1.0* counts[i] / counts[i-1]: 100;
-  }
+  int frame_num = i;
+  cout << "Video contains" << frame_num << " frames" <<endl;
+  MyImage tomograph;
+  tomograph.setWidth(Width);
+  tomograph.setHeight(frame_num);
+  tomograph.setImagePath("tomograph.rgb");
 
-  vector<float> ddc(num_frame, 0);
-  ofstream myfile1;
-  ofstream myfile2;
-  myfile1.open ("result-y.txt");
-  myfile2.open ("result-x.txt");
+  char * tomoData = new char[Width*i*3];
 
-  for(int i = 2; i < num_frame; i++) {
-    ddc[i] = dc[i-1] !=0? dc[i] / dc[i-1] : 100;
-    myfile1<<ddc[i]<<endl;
-    myfile2<<i * 0.033<<endl;
+  for(int row = 0; row < frame_num; row ++) {
+    for(int col = 0; col < Width; col ++) {
+      int pos = row * Width * 3 + col * 3;
+      tomoData[pos] = tomoData_r[row][col];
+      tomoData[pos + 1] = tomoData_g[row][col];
+      tomoData[pos + 2] = tomoData_b[row][col];
+    }
   }
-  myfile1.close();
-  myfile2.close();
 
   tomograph.setImageData(tomoData);
   tomograph.WriteImage();
@@ -119,55 +157,39 @@ bool ShotBoundaryDetector::GetAvgRow(MyImage& input_image, char* output_row) {
     double sum_r = 0;
     double sum_g = 0;
     double sum_b = 0;
+    int rows = 0;
     for(int row = 0; row < Height; row++) {
+      rows ++;
       int position = row * Width * 3 + col * 3;
       sum_r += Data[position];
       sum_g += Data[position + 1];
       sum_b += Data[position + 2];
 		}
-    output_row[col * 3] = (char)( min<double>(255,max<double>(sum_r / Height, 0)) );
-    output_row[col * 3 + 1] = (char)( min<double>(255,max<double>(sum_g / Height, 0)) );
-    output_row[col * 3 + 2] = (char)( min<double>(255,max<double>(sum_b / Height, 0)) );
+    output_row[col * 3] = (char)( min<double>(255,max<double>(sum_r / rows, 0)) );
+    output_row[col * 3 + 1] = (char)( min<double>(255,max<double>(sum_g / rows, 0)) );
+    output_row[col * 3 + 2] = (char)( min<double>(255,max<double>(sum_b / rows, 0)) );
   }
 
   return true;
 }
 
 bool ShotBoundaryDetector::ReadNextFrame(MyImage& output_image, FILE* fp) {
-    	// Create and populate RGB buffers
-    	int i;
-    	char *Rbuf = new char[Height*Width];
-    	char *Gbuf = new char[Height*Width];
-    	char *Bbuf = new char[Height*Width];
 
       char *Data   = new char[Width*Height*3];
 
-    	for (i = 0; i < Width*Height; i ++)
+      // BGR for testing on windows
+    	for (int i = 0; i < Width*Height; i ++)
     	{
-    		Rbuf[i] = fgetc(fp);
+    			Data[3*i+2] = fgetc(fp);
     	}
-    	for (i = 0; i < Width*Height; i ++)
+    	for (int i = 0; i < Width*Height; i ++)
     	{
-    		Gbuf[i] = fgetc(fp);
+    		  Data[3*i+1]	= fgetc(fp);
     	}
-    	for (i = 0; i < Width*Height; i ++)
+    	for (int i = 0; i < Width*Height; i ++)
     	{
-    		Bbuf[i] = fgetc(fp);
+    		  Data[3*i] = fgetc(fp);
     	}
-
-    	// Allocate Data structure and copy
-    	Data = new char[Width*Height*3];
-    	for (i = 0; i < Height*Width; i++)
-    	{
-    		Data[3*i]	= Bbuf[i];
-    		Data[3*i+1]	= Gbuf[i];
-    		Data[3*i+2]	= Rbuf[i];
-    	}
-
-    	// Clean up and return
-    	delete[] Rbuf;
-    	delete[] Gbuf;
-    	delete[] Bbuf;
 
       output_image.setImageData(Data);
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "Detector.h"
 #include "Image.h"
-
+#include <ctime>
 int main() {
   ShotBoundaryDetector detector("./dataset/ad.rgb", 480, 270);
   // ShotBoundaryDetector detector("./dataset/Videos/data_test1.rgb", 480, 270);
-  detector.StartDetection();
+  int start_s=clock();
+	detector.StartDetection();
+  int stop_s=clock();
+  cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << endl;
   return 0;
 }
